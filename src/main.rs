d2e6358952106de2986643706f8f45918c369809
Madcowe <34983336+Madcowe@users.noreply.github.com>
2025-03-30 13:56:22 +0100

    Add download after uploads so local scratchpad in sync

--- src/main.rs
+++ src/main.rs
@@ -46,16 +46,22 @@ async fn run() -> Result<()> {
         counter_app.print_counter_state();
     }
     println!("{:?}", counter_app.counter);
-    match counter_app.counter_state {
-        CounterState::Connected { ref scratchpad, .. } => println!("{:?}", scratchpad),
-        _ => (),
+    if counter_app.is_connected().await {
+        counter_app.download().await?;
+        counter_app.print_scratchpad()?;
     }
+    // match counter_app.counter_state {
+    //     CounterState::Connected { ref scratchpad, .. } => println!("{:?}", scratchpad),
+    //     _ => (),
+    // }
     // reset counter if needed
     match counter_app.counter.reset_if_next_period()? {
-        true => counter_app.upload().await?,
+        true => {
+            counter_app.upload().await?;
+            println!("{:?}", counter_app.counter);
+        }
         _ => (),
     }
-    println!("{:?}", counter_app.counter);
     // loop asking user for value to store and then storing on scratch pad
     loop {
         // get input from user
@@ -71,7 +77,12 @@ async fn run() -> Result<()> {
         }
         // reset counter if needed
         match counter_app.counter.reset_if_next_period()? {
-            true => counter_app.upload().await?,
+            true => {
+                println!("Reset gubbins");
+                counter_app.upload().await?;
+                counter_app.download().await?; // so local scratchpad synced
+                counter_app.print_scratchpad()?;
+            }
             _ => (),
         }
         // if not connected but have been connected update to local counter
@@ -80,16 +91,18 @@ async fn run() -> Result<()> {
             "i" => {
                 counter_app.counter.increment();
                 counter_app.upload().await?;
+                counter_app.download().await?; // so local scratchpad synced
                 counter_app.print_scratchpad()?;
             }
             "r" => {
                 counter_app.counter.reset();
                 counter_app.upload().await?;
+                counter_app.download().await?; // so local scratchpad synced
                 counter_app.print_scratchpad()?;
             }
             "q" => break,
             _ => {
-                println!("Inrecognised commad");
+                println!("Inrecognised command");
                 continue;
             }
         }
@@ -112,95 +125,3 @@ async fn get_funded_wallet() -> Result<Wallet> {
     println!("Wallet ballance: {}", wallet.balance_of_tokens().await?);
     Ok(wallet)
 }
-
-// // loop asking user for value to store and then storing on scratch pad
-// loop {
-//     // get input from user
-//     println!("Enter i to increment counter, r to reset or q to quit:");
-//     let mut input = String::new();
-//     io::stdin().read_line(&mut input)?;
-//     let input = input.trim();
-
-//     // sync counter with scratchpad and reset if needed
-//     counter = get_scratchpad_counter(&client, &scratchpad, &key).await?;
-//     match counter.reset_if_next_period()? {
-//         true => {
-//             // if counter is reset upload
-//             scratchpad = update_scratchpad_counter(&client, &scratchpad, &counter, &key).await?
-//         }
-//         _ => (),
-//     }
-//     // println!("{:?}", counter);
-
-//     match input {
-//         "i" => {
-//             // download data again incase it has been changed by another app
-//             counter = get_scratchpad_counter(&client, &scratchpad, &key).await?;
-//             counter.increment();
-//         }
-//         "r" => {
-//             // download data again incase it has been changed by another app
-//             counter = get_scratchpad_counter(&client, &scratchpad, &key).await?;
-//             counter.reset();
-//         }
-//         "q" => break,
-//         _ => {
-//             println!("Unrecognised command");
-//             continue;
-//         }
-//     }
-//     // upload to antnet
-//     scratchpad = update_scratchpad_counter(&client, &scratchpad, &counter, &key).await?;
-//     // downlaod and print results
-//     counter = bincode::deserialize(&scratchpad.decrypt_data(&key)?)?;
-//     println!(
-//         "scratchpad version {:?}, value: {:?}",
-//         scratchpad.counter(),
-//         counter
-//     );
-// }
-
-// fn create_key(path: &Path) -> Result<autonomi::SecretKey> {
-//     let key = autonomi::SecretKey::random();
-//     let key_hex = key.to_hex();
-//     println!("New key: {}", key_hex);
-//     let mut file = File::create_new(&path)?;
-//     file.write_all(key_hex.as_bytes())?;
-//     Ok(key)
-// }
-
-// async fn get_scratchpad_counter(
-//     client: &Client,
-//     scratchpad: &Scratchpad,
-//     key: &autonomi::SecretKey,
-// ) -> Result<Counter> {
-//     let counter: Counter = bincode::deserialize(
-//         &client
-//             .scratchpad_get(&scratchpad.address())
-//             .await?
-//             .decrypt_data(&key)?,
-//     )?;
-//     Ok(counter)
-// }
-
-// async fn update_scratchpad_counter(
-//     client: &Client,
-//     scratchpad: &Scratchpad,
-//     counter: &Counter,
-//     key: &autonomi::SecretKey,
-// ) -> Result<Scratchpad> {
-//     println!("{:?}", counter);
-//     println!("Syncing to ant network...");
-//     let counter_serailzed = bincode::serialize(&counter)?;
-//     let content = Bytes::from(counter_serailzed);
-//     let content_type = 99;
-//     client
-//         .scratchpad_update(&key, content_type, &content)
-//         .await?;
-//     while *counter != get_scratchpad_counter(&client, &scratchpad, &key).await? {
-//         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-//         println!("Syncing to ant network...");
-//     }
-//     println!("Synced");
-//     Ok(client.scratchpad_get(scratchpad.address()).await?)
-// }
